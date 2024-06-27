dc23f37f0393e945d87de5038b03d88089fc8fc4
DefaultDebugger16 <92399838+DefaultDebugger16@users.noreply.github.com>
2024-06-27 16:46:48 -0400

    Add files via upload
    you'll need to download react + some other dependencies and drop these in the src folder

---
+++ App.js
@@ -0,0 +1,19 @@
+// src/App.js
+import React from 'react';
+//import './App.css';
+import Chatbot from './Chatbot';
+import './Chatbot.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Chatbot</h1>
+        <Chatbot />
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ HistoryBar.js
@@ -0,0 +1,9 @@
+const chatTab = () => {
+    return (
+        <div className="tab">
+            <button>Chat n</button>
+        </div>
+    );
+}
+ 
+export default chatTab;
